0db61f7d93a97f7f5e96d5b19e560d3c0464d8b1
Anushervon Khidirov <ankhid98@gmail.com>
2025-08-29 17:21:53 +0500

    files service added

---
+++ src/services/files/files.service.ts
@@ -0,0 +1,50 @@
+import {
+  existsSync,
+  mkdirSync,
+  copyFileSync,
+  readFileSync,
+  writeFileSync,
+  readdirSync,
+  watch,
+} from 'fs';
+
+type CreateFileOptions = {
+  showError: boolean;
+};
+
+export class FileService {
+  static createFolder(path: string) {
+    if (!existsSync(path)) mkdirSync(path, { recursive: true });
+  }
+
+  static createFile(path: string, data: string = '', options?: CreateFileOptions) {
+    const isExist = existsSync(path);
+
+    if (isExist && options?.showError) throw `file already exists: ${path}`;
+    if (!isExist) writeFileSync(path, data, { encoding: 'utf-8' });
+  }
+
+  static readFile(path: string) {
+    if (!existsSync(path)) throw `Path not found: ${path}`;
+    return readFileSync(path, { encoding: 'utf-8' });
+  }
+
+  static writeFile(path: string, data: string) {
+    if (!existsSync(path)) throw `Path not found: ${path}`;
+    writeFileSync(path, data, { encoding: 'utf-8' });
+  }
+
+  static readDir(path: string) {
+    if (!existsSync(path)) throw `Path not found: ${path}`;
+    return readdirSync(path, { encoding: 'utf-8' });
+  }
+
+  static copy(pathFrom: string, pathTo: string) {
+    if (!existsSync(pathFrom)) throw `Path not found: ${pathFrom}`;
+    if (!existsSync(pathTo)) this.createFolder(pathTo.replace(/[\w-]+\.[A-Za-z0-9]{2,5}/g, ''));
+
+    copyFileSync(pathFrom, pathTo);
+  }
+
+  static watchFile() {}
+}
